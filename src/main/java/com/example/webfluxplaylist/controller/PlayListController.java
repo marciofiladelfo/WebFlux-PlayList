c60d9282cac4192be72cc4fda5defdd0f179b957
marciofiladelfo <marcio.kabana@gmail.com>
2023-03-03 19:14:14 -0300

    Criado controller

---
+++ src/main/java/com/example/webfluxplaylist/controller/PlayListController.java
@@ -0,0 +1,51 @@
+package com.example.webfluxplaylist.controller;
+
+import com.example.webfluxplaylist.domain.PlayList;
+import com.example.webfluxplaylist.service.PlayListServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.Sinks;
+
+@RestController
+@RequestMapping("/playlist")
+@Slf4j
+public class PlayListController {
+
+    private final PlayListServiceImpl service;
+
+    public PlayListController(PlayListServiceImpl playListService) {
+        this.service = playListService;
+    }
+
+    Sinks.Many<PlayList> sinks = Sinks.many().replay().latest();
+
+    @GetMapping
+    public Flux<PlayList> listar() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Mono<ResponseEntity<PlayList>> buscarPorId(@PathVariable String id) {
+        Mono<PlayList> buscar = service.findById(id);
+        return buscar
+                .map(element -> ResponseEntity.ok().body(element))
+                .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<PlayList> salvar(@RequestBody PlayList playList) {
+        return service.save(playList)
+                .doOnNext(element -> sinks.tryEmitNext(element));
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<Void> apagar(@PathVariable String id) {
+        return service.remover(id);
+    }
+
+}
