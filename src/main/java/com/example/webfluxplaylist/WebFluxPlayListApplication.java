c90b2ed6af1cedac9e6d3a23bd8bee81ae820fd5
marciofiladelfo <marcio.kabana@gmail.com>
2023-03-03 18:54:22 -0300

    Initial commit

---
+++ src/main/java/com/example/webfluxplaylist/WebFluxPlayListApplication.java
@@ -0,0 +1,13 @@
+package com.example.webfluxplaylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxPlayListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebFluxPlayListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webfluxplaylist/WebFluxPlayListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webfluxplaylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxPlayListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
